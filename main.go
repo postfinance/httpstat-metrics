c262b6918a39637cf8ff80337c6e16abb7528de1
Clément Nussbaumer <clement.nussbaumer@postfinance.ch>
2022-12-06 15:37:27 +0100

    feat: basic queries and timing

--- main.go
+++ main.go
@@ -6,13 +6,10 @@ import (
 	"encoding/pem"
 	"flag"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"log"
-	"mime"
 	"net"
 	"net/http"
-	"net/http/httptrace"
 	"net/url"
 	"os"
 	"runtime"
@@ -30,7 +27,6 @@ var (
 	followRedirects bool
 	onlyHeader      bool
 	insecure        bool
-	httpHeaders     headers
 	saveOutput      bool
 	outputFile      string
 	showVersion     bool
@@ -49,16 +45,14 @@ const maxRedirects = 10
 
 func init() {
 	flag.StringVar(&configFile, "config", "config.yaml", "path to the configuration file")
-	flag.StringVar(&httpMethod, "X", "GET", "HTTP method to use")
 	flag.BoolVar(&insecure, "k", false, "allow insecure SSL connections")
-	flag.Var(&httpHeaders, "H", "set HTTP header; repeatable: -H 'Accept: ...' -H 'Range: ...'")
 	flag.BoolVar(&saveOutput, "O", false, "save body as remote filename")
 	flag.StringVar(&outputFile, "o", "", "output file for body")
 	flag.BoolVar(&showVersion, "v", false, "print version number")
 	flag.StringVar(&clientCertFile, "E", "", "client cert file for tls config")
 	flag.BoolVar(&fourOnly, "4", false, "resolve IPv4 addresses only")
 	flag.BoolVar(&sixOnly, "6", false, "resolve IPv6 addresses only")
-	flag.DurationVar(&interval, "interval", 5*time.Second, "interval between http queries. must be in Go time.ParseDuration format, e.g. 5s or 5m or 1h, etc")
+	flag.DurationVar(&interval, "interval", 1*time.Second, "interval between http queries. must be in Go time.ParseDuration format, e.g. 5s or 5m or 1h, etc")
 
 	flag.Usage = usage
 }
@@ -183,14 +177,6 @@ func parseURL(uri string) *url.URL {
 	return url
 }
 
-func headerKeyValue(h string) (string, string) {
-	i := strings.Index(h, ":")
-	if i == -1 {
-		log.Fatalf("Header '%s' has invalid format, missing ':'", h)
-	}
-	return strings.TrimRight(h[:i], " "), strings.TrimLeft(h[i:], " :")
-}
-
 func dialContext(network string) func(ctx context.Context, network, addr string) (net.Conn, error) {
 	return func(ctx context.Context, _, addr string) (net.Conn, error) {
 		return (&net.Dialer{
@@ -199,206 +185,4 @@ func dialContext(network string) func(ctx context.Context, network, addr string)
 			DualStack: false,
 		}).DialContext(ctx, network, addr)
 	}
-}
-
-// visit visits a url and times the interaction.
-// If the response is a 30x, visit follows the redirect.
-func visit(url *url.URL) {
-	req := newRequest(httpMethod, url, postBody)
-
-	var getConnTime, dnsStartTime, dnsDoneTime, connectStartTime, connectDoneTime, gotConnTime, gotFirstResponseByteTime, tlsHandshakeStartTime, tlsHandshakeStopTime time.Time
-
-	trace := &httptrace.ClientTrace{
-		GetConn:  func(hostPort string) { getConnTime = time.Now() },
-		DNSStart: func(_ httptrace.DNSStartInfo) { dnsStartTime = time.Now() },
-		DNSDone:  func(_ httptrace.DNSDoneInfo) { dnsDoneTime = time.Now() },
-		ConnectStart: func(_, _ string) {
-			if connectStartTime.IsZero() {
-				// connecting to IP
-				connectStartTime = time.Now()
-			}
-		},
-		ConnectDone: func(net, addr string, err error) {
-			if err != nil {
-				log.Fatalf("unable to connect to host %v: %v", addr, err)
-			}
-			connectDoneTime = time.Now()
-
-		},
-		GotConn:              func(_ httptrace.GotConnInfo) { gotConnTime = time.Now() },
-		GotFirstResponseByte: func() { gotFirstResponseByteTime = time.Now() },
-		TLSHandshakeStart:    func() { tlsHandshakeStartTime = time.Now() },
-		TLSHandshakeDone:     func(_ tls.ConnectionState, _ error) { tlsHandshakeStopTime = time.Now() },
-	}
-	req = req.WithContext(httptrace.WithClientTrace(context.Background(), trace))
-
-	tr := &http.Transport{
-		Proxy:                 http.ProxyFromEnvironment,
-		MaxIdleConns:          100,
-		IdleConnTimeout:       90 * time.Second,
-		TLSHandshakeTimeout:   10 * time.Second,
-		ExpectContinueTimeout: 1 * time.Second,
-		ForceAttemptHTTP2:     true,
-	}
-
-	switch {
-	case fourOnly:
-		tr.DialContext = dialContext("tcp4")
-	case sixOnly:
-		tr.DialContext = dialContext("tcp6")
-	}
-
-	switch url.Scheme {
-	case "https":
-		host, _, err := net.SplitHostPort(req.Host)
-		if err != nil {
-			host = req.Host
-		}
-
-		tr.TLSClientConfig = &tls.Config{
-			ServerName:         host,
-			InsecureSkipVerify: insecure,
-			Certificates:       readClientCert(clientCertFile),
-			MinVersion:         tls.VersionTLS12,
-		}
-	}
-
-	client := &http.Client{
-		Transport: tr,
-		CheckRedirect: func(req *http.Request, via []*http.Request) error {
-			// always refuse to follow redirects, visit does that
-			// manually if required.
-			return http.ErrUseLastResponse
-		},
-	}
-
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatalf("failed to read response: %v", err)
-	}
-
-	bodySize, _ := io.Copy(io.Discard, resp.Body)
-	resp.Body.Close()
-
-	postBodyReadTime := time.Now() // after read body
-	if dnsStartTime.IsZero() {
-		// we skipped DNS
-		dnsStartTime = dnsDoneTime
-	}
-
-	dnsLookup := dnsDoneTime.Sub(dnsStartTime)
-	tcpConnection := connectDoneTime.Sub(connectStartTime)
-	tlsHandshake := tlsHandshakeStopTime.Sub(tlsHandshakeStartTime)
-	serverProcessing := gotFirstResponseByteTime.Sub(gotConnTime)
-	contentTransfer := postBodyReadTime.Sub(gotFirstResponseByteTime)
-	totalDuration := postBodyReadTime.Sub(getConnTime)
-
-	fmt.Print(bodySize, dnsLookup, tcpConnection, tlsHandshake, serverProcessing, contentTransfer, totalDuration)
-	// httpstat_error_total{code="404"}
-	// httpstat_lookup_total{code="404"}
-
-}
-
-func newRequest(method string, url *url.URL, body string) *http.Request {
-	req, err := http.NewRequest(method, url.String(), createBody(body))
-	if err != nil {
-		log.Fatalf("unable to create request: %v", err)
-	}
-	for _, h := range httpHeaders {
-		k, v := headerKeyValue(h)
-		if strings.EqualFold(k, "host") {
-			req.Host = v
-			continue
-		}
-		req.Header.Add(k, v)
-	}
-	return req
-}
-
-func createBody(body string) io.Reader {
-	if strings.HasPrefix(body, "@") {
-		filename := body[1:]
-		f, err := os.Open(filename)
-		if err != nil {
-			log.Fatalf("failed to open data file %s: %v", filename, err)
-		}
-		return f
-	}
-	return strings.NewReader(body)
-}
-
-// getFilenameFromHeaders tries to automatically determine the output filename,
-// when saving to disk, based on the Content-Disposition header.
-// If the header is not present, or it does not contain enough information to
-// determine which filename to use, this function returns "".
-func getFilenameFromHeaders(headers http.Header) string {
-	// if the Content-Disposition header is set parse it
-	if hdr := headers.Get("Content-Disposition"); hdr != "" {
-		// pull the media type, and subsequent params, from
-		// the body of the header field
-		mt, params, err := mime.ParseMediaType(hdr)
-
-		// if there was no error and the media type is attachment
-		if err == nil && mt == "attachment" {
-			if filename := params["filename"]; filename != "" {
-				return filename
-			}
-		}
-	}
-
-	// return an empty string if we were unable to determine the filename
-	return ""
-}
-
-type headers []string
-
-func (h headers) String() string {
-	var o []string
-	for _, v := range h {
-		o = append(o, "-H "+v)
-	}
-	return strings.Join(o, " ")
-}
-
-func (h *headers) Set(v string) error {
-	*h = append(*h, v)
-	return nil
-}
-
-func (h headers) Len() int      { return len(h) }
-func (h headers) Swap(i, j int) { h[i], h[j] = h[j], h[i] }
-func (h headers) Less(i, j int) bool {
-	a, b := h[i], h[j]
-
-	// server always sorts at the top
-	if a == "Server" {
-		return true
-	}
-	if b == "Server" {
-		return false
-	}
-
-	endtoend := func(n string) bool {
-		// https://www.w3.org/Protocols/rfc2616/rfc2616-sec13.html#sec13.5.1
-		switch n {
-		case "Connection",
-			"Keep-Alive",
-			"Proxy-Authenticate",
-			"Proxy-Authorization",
-			"TE",
-			"Trailers",
-			"Transfer-Encoding",
-			"Upgrade":
-			return false
-		default:
-			return true
-		}
-	}
-
-	x, y := endtoend(a), endtoend(b)
-	if x == y {
-		// both are of the same class
-		return a < b
-	}
-	return x
 }
--- querier.go
+++ querier.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"context"
+	"crypto/tls"
 	"fmt"
+	"io"
+	"log"
 	"math/rand"
+	"net"
+	"net/http"
+	"net/http/httptrace"
 	"net/url"
 	"time"
 
@@ -17,9 +24,35 @@ type Querier struct {
 	httpServerConfig *HTTPServerConfig
 	labels           string
 	url              url.URL
+	tlsClientConfig  *tls.Config
+	tr               *http.Transport
 }
 
 func (q *Querier) Run(interval *time.Duration) {
+
+	q.tr = &http.Transport{
+		Proxy:                 http.ProxyFromEnvironment,
+		MaxIdleConns:          100,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+		ForceAttemptHTTP2:     true,
+	}
+
+	switch q.url.Scheme {
+	case "https":
+		host, _, err := net.SplitHostPort(q.url.Host)
+		if err != nil {
+			host = q.url.Host
+		}
+
+		q.tr.TLSClientConfig = &tls.Config{
+			ServerName:         host,
+			InsecureSkipVerify: insecure,
+			Certificates:       readClientCert(clientCertFile),
+			MinVersion:         tls.VersionTLS12,
+		}
+	}
 
 	labelsMap := q.httpServerConfig.ExtraLabels
 	labelsMap["host"] = q.url.Host
@@ -35,7 +68,6 @@ func (q *Querier) Run(interval *time.Duration) {
 	q.labels += "}"
 
 	metrics.GetOrCreateCounter(fmt.Sprintf("%s%s", dnsLookupDuration, q.labels)).Set(0)
-	metrics.GetOrCreateCounter(fmt.Sprintf("%s%s", dnsLookupDuration, q.labels)).Set(0)
 
 	//nolint:gosec // No need for a cryptographic secure random number since this is only used for a jitter.
 	jitter := time.Duration(rand.Float64() * float64(500*time.Millisecond))
@@ -50,6 +82,92 @@ func (q *Querier) Run(interval *time.Duration) {
 	defer ticker.Stop()
 
 	for range ticker.C {
-		visit(&q.url)
+		q.visit()
 	}
+}
+
+// visit visits a url and times the interaction.
+// If the response is a 30x, visit follows the redirect.
+func (q *Querier) visit() {
+	req, err := http.NewRequest("GET", q.url.String(), nil)
+	if err != nil {
+		log.Fatalf("unable to create request: %v", err)
+	}
+	req.Header = q.httpServerConfig.HttpHeaders
+
+	var getConnTime, dnsStartTime, dnsDoneTime, connectStartTime, connectDoneTime, gotConnTime, gotFirstResponseByteTime, tlsHandshakeStartTime, tlsHandshakeStopTime time.Time
+
+	trace := &httptrace.ClientTrace{
+		GetConn:  func(hostPort string) { getConnTime = time.Now() },
+		DNSStart: func(_ httptrace.DNSStartInfo) { dnsStartTime = time.Now() },
+		DNSDone:  func(_ httptrace.DNSDoneInfo) { dnsDoneTime = time.Now() },
+		ConnectStart: func(_, _ string) {
+			if connectStartTime.IsZero() {
+				// connecting to IP
+				connectStartTime = time.Now()
+			}
+		},
+		ConnectDone: func(net, addr string, err error) {
+			if err != nil {
+				log.Fatalf("unable to connect to host %v: %v", addr, err)
+			}
+			connectDoneTime = time.Now()
+
+		},
+		GotConn:              func(_ httptrace.GotConnInfo) { gotConnTime = time.Now() },
+		GotFirstResponseByte: func() { gotFirstResponseByteTime = time.Now() },
+		TLSHandshakeStart:    func() { tlsHandshakeStartTime = time.Now() },
+		TLSHandshakeDone:     func(_ tls.ConnectionState, _ error) { tlsHandshakeStopTime = time.Now() },
+	}
+	req = req.WithContext(httptrace.WithClientTrace(context.Background(), trace))
+
+	switch {
+	case fourOnly:
+		q.tr.DialContext = dialContext("tcp4")
+	case sixOnly:
+		q.tr.DialContext = dialContext("tcp6")
+	default:
+		q.tr.DialContext = dialContext("tcp")
+	}
+
+	switch q.url.Scheme {
+	case "https":
+		q.tr.TLSClientConfig = &tls.Config{
+			InsecureSkipVerify: insecure,
+			Certificates:       readClientCert(clientCertFile),
+			MinVersion:         tls.VersionTLS12,
+		}
+	}
+
+	client := &http.Client{
+		Transport: q.tr,
+	}
+
+	defer q.tr.CloseIdleConnections()
+	resp, err := client.Do(req)
+	if err != nil {
+		// log.Fatalf("failed to read response: %v", err)
+		return
+	}
+
+	bodySize, _ := io.Copy(io.Discard, resp.Body)
+	resp.Body.Close()
+
+	postBodyReadTime := time.Now() // after read body
+	if dnsStartTime.IsZero() {
+		// we skipped DNS
+		dnsStartTime = dnsDoneTime
+	}
+
+	dnsLookup := dnsDoneTime.Sub(dnsStartTime)
+	tcpConnection := connectDoneTime.Sub(connectStartTime)
+	tlsHandshake := tlsHandshakeStopTime.Sub(tlsHandshakeStartTime)
+	serverProcessing := gotFirstResponseByteTime.Sub(gotConnTime)
+	contentTransfer := postBodyReadTime.Sub(gotFirstResponseByteTime)
+	totalDuration := postBodyReadTime.Sub(getConnTime)
+
+	fmt.Print(bodySize, dnsLookup, tcpConnection, tlsHandshake, serverProcessing, contentTransfer, totalDuration)
+	// httpstat_error_total{code="404"}
+	// httpstat_lookup_total{code="404"}
+
 }
